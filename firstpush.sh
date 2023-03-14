63e36490106a8d7496526bcb64966e319ed81472
Seanlau <lausean561@gmail.com>
2023-03-14 20:06:13 +0800

    feat:update

---
+++ firstpush.sh
@@ -0,0 +1,6 @@
+git init &&
+git add . &&
+git commit -m "first commit" &&
+git checkout -b repo-branch &&
+git remote add origin git@github.com:Seanlau01/pineapple-ui.git &&
+git push -u origin repo-branch &&
